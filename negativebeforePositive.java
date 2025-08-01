31aa1a4d3a69c5f470135b12d2f2ab653c3a2471
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-08-01 22:17:14 +0530

    placing all negative no.s before positive no.s

--- negativebeforePositive.java
+++ negativebeforePositive.java
@@ -2,9 +2,28 @@ import java.util.Arrays;
 
 public class negativebeforePositive {
     public static void main(String[] args) {
-        int[] nums = {-4, 8, 6, -5, 6, -2, 1, 2, 3, -11};
-        Arrays.sort(nums);
-        System.out.println("modified arrays is : " + Arrays.toString(nums));
+         int[] nums = {-4, 8, 6, -5, -6, -2, 1, 2, 3, -11};
+
+        int[] result = new int[nums.length];
+        int index = 0;
+
+        //  add all negative numbers
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                result[index] = nums[i];
+                index++;
+            }
+        }
+
+        // add all positive numbers (and 0 if needed)
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                result[index] = nums[i];
+                index++;
+            }
+        }
+
+        System.out.println("modified arrays is : " + Arrays.toString(result));
 
     }
     
