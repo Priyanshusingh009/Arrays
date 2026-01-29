2cb7367544f289a84ad0281c154f3650a1a81c9e
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2026-01-29 21:51:58 +0530

    finding second largest element in array

---
+++ secondLargest.java
@@ -0,0 +1,18 @@
+public class secondLargest {
+    public static void main(String[] args) {
+        int[] arr={34,67,23,89,2,45,90,12};
+        int firstLargest=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>firstLargest){
+                secondLargest=firstLargest;
+                firstLargest=arr[i];
+            }
+            else if(arr[i]>secondLargest && arr[i]!=firstLargest){
+                secondLargest=arr[i];
+            }
+        }
+        System.out.println("The second largest element is: "+secondLargest);
+    }
+    
+}
